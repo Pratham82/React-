1bf9a35d365fa75c24acdf2928b8aa9cc2a02744
Pratham82 <mali.prathamesh82@gmail.com>
2020-06-29 18:34:37 +0530

    Main files modified

--- FreeCodeCamp (Front End Libraries)/fcc-react/src/index.js
+++ FreeCodeCamp (Front End Libraries)/fcc-react/src/index.js
@@ -4,19 +4,39 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-const JSX = (
-	<div className="myDiv">
-		<h1>Hello World</h1>
-		<p>Lets render this to the DOM</p>
-	</div>
-);
-ReactDOM.render(JSX, document.getElementById("root"));
+// All HTML elements should be wrapped in parent elements or else two contiguous divs will cause error if there is no parent element wrapping them around
+// const JSX = (
+// 	<body>
+// 		<div>
+// 			<h1>Hello World</h1>
+// 			<p>Lets render this to the DOM</p>
+// 		</div>
+// 		<div>This is testing string</div>
+// 	</body>
+// );
+//<App />
+
+// This component can be added ad <ChildComponent/ inside render methfo>
+const ChildComponent = () => <h3>This is child component</h3>;
+
+class MyComponent extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return (
+			<div>
+				<h1>Hello React!</h1>
+				<ChildComponent />
+			</div>
+		);
+	}
+}
+//Using this component: <MyComponent />
+const root = document.getElementById("root");
+ReactDOM.render(<MyComponent />, root);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-// <React.StrictMode>
-// 	<App />
-// </React.StrictMode>
