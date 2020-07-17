b3a6d091b951bb3fcd69921b66efff1cf0e02542
Pratham82 <mali.prathamesh82@gmail.com>
2020-07-17 22:02:15 +0530

    Day 1 of React course

---
+++ React (Tyler McGinnis course)/Gihub battle/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "../Daily files/Day1/Day 1 Expressions";
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ React (Tyler McGinnis course)/Gihub battle/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const webpack = require("webpack");
+
+module.exports = {
+	entry: "./src/index.js",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "bundle.js",
+	},
+	module: {
+		rules: [
+			{ test: /\.svg$/, use: "svg-inline-loader" },
+			{ test: /\.css$/, use: ["style-loader", "css-loader"] },
+			{ test: /\.js$/, use: "babel-loader" },
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "src/index.html",
+		}),
+		// new webpack.EnvironmentPlugin({
+		// 	NODE_ENV: "production",
+		// }),
+	],
+	mode: "development",
+};
