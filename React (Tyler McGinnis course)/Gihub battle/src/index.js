acec68de325e6383a14ef586f6996c223b16b621
Pratham82 <mali.prathamesh82@gmail.com>
2020-07-18 14:01:42 +0530

    App chnaged

--- React (Tyler McGinnis course)/Gihub battle/src/index.js
+++ React (Tyler McGinnis course)/Gihub battle/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "../Daily files/Day1/Day 1 Expressions";
+import App from "../Daily files/Day 2/Props/App2";
 
 ReactDOM.render(<App />, document.getElementById("root"));
