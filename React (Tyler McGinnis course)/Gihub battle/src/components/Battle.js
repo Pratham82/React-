e5b704531874e01073a0700b5f645fd21b4f1aca
Pratham82 <mali.prathamesh82@gmail.com>
2020-07-23 17:23:40 +0530

    Added Player input component

--- React (Tyler McGinnis course)/Gihub battle/src/components/Battle.js
+++ React (Tyler McGinnis course)/Gihub battle/src/components/Battle.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { FaUserFriends, FaFighterJet, FaTrophy } from "react-icons/fa";
+import PropTypes from "prop-types";
 
 function Instructions() {
 	return (
@@ -35,12 +36,80 @@ function Instructions() {
 	);
 }
 
+class PlayerInput extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			username: "",
+		};
+
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleSubmit(event) {
+		event.preventDefault();
+		this.props.onSubmit(this.state.username);
+	}
+
+	handleChange(event) {
+		this.setState({
+			username: event.target.value,
+		});
+	}
+
+	render() {
+		return (
+			<form className="column player" onSubmit={this.handleSubmit}>
+				<label htmlFor="username" className="player-label">
+					{this.props.label}
+				</label>
+				<div className="row player-inputs">
+					<input
+						type="text"
+						id="username"
+						className="input-light"
+						placeholder="github username"
+						autoComplete="off"
+						value={this.state.username}
+						onChange={this.handleChange}
+					/>
+
+					<button
+						className="btn btn-dark"
+						type="submit"
+						disabled={!this.state.username}
+					>
+						Submit
+					</button>
+				</div>
+			</form>
+		);
+	}
+}
+
+PlayerInput.propTypes = {
+	onSubmit: PropTypes.func.isRequired,
+	label: PropTypes.string.isRequired,
+};
+
 export default class Battle extends React.Component {
 	render() {
 		return (
 			<React.Fragment>
 				<Instructions />
+				<PlayerInput
+					label="User 1"
+					onSubmit={(value) => console.log(value)}
+				/>
 			</React.Fragment>
 		);
 	}
 }
+
+/*
+* Whenever you have a controlled component, the value in the input field is going to e whatever	is on the local state, that means if we want to update text of input field then we have to update the local state
+
+* Submit button will only be enabled when there is a value inside of the input field
+*/
