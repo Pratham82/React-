d9148f8f056e5522fe6f54236d6038b73cc4a3a1
Pratham82 <mali.prathamesh82@gmail.com>
2020-08-01 11:59:50 +0530

    Added hover component

--- React (Tyler McGinnis course)/Gihub battle/src/components/Tooltip.js
+++ React (Tyler McGinnis course)/Gihub battle/src/components/Tooltip.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Hover from "./Hover";
 
 const styles = {
 	container: {
@@ -23,44 +24,23 @@ const styles = {
 	},
 };
 
-export default class Tooltip extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			hovering: false,
-		};
-
-		this.mouseOver = this.mouseOver.bind(this);
-		this.mouseOut = this.mouseOut.bind(this);
-	}
-
-	mouseOver() {
-		this.setState({
-			hovering: true,
-		});
-	}
-
-	mouseOut() {
-		this.setState({
-			hovering: false,
-		});
-	}
-	render() {
-		const { text, children } = this.props;
-		const { hovering } = this.state;
-		return (
-			<div
-				onMouseOver={this.mouseOver}
-				onMouseOut={this.mouseOut}
-				style={styles.container}
-			>
-				{hovering === true && <div style={styles.tooltip}>{text}</div>}
-				{children}
-			</div>
-		);
-	}
+export default function Tooltip({ text, children, hovering }) {
+	return (
+		<Hover>
+			{(hovering) => (
+				<div style={styles.container}>
+					{hovering === true && (
+						<div style={styles.tooltip}>{text}</div>
+					)}
+					{children}
+				</div>
+			)}
+		</Hover>
+	);
 }
 
 Tooltip.propTypes = {
 	text: PropTypes.string.isRequired,
 };
+
+//* Higher order component is just a component it takes in another component as its argument it returns new component and the component it returns can render the original component that was passed, in this case the tooltip component
