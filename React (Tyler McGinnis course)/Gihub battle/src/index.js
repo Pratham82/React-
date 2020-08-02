bbc1dd98d89f3c18645831c8d542c89da78b20aa
Pratham82 <mali.prathamesh82@gmail.com>
2020-08-02 16:37:05 +0530

    Added Nav component to the main component

--- React (Tyler McGinnis course)/Gihub battle/src/index.js
+++ React (Tyler McGinnis course)/Gihub battle/src/index.js
@@ -3,17 +3,37 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import Popular from "./components/Popular";
 import Battle from "./components/Battle";
-
+import { ThemeProvider } from "./contexts/theme";
+import Nav from "./components/Nav";
 class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			theme: "light",
+			toggleTheme: () => {
+				this.setState(({ theme }) => ({
+					theme: theme === "light" ? "dark" : "light",
+				}));
+			},
+		};
+	}
+
 	render() {
 		// let friends = this.props.friends;
 		// console.log(friends);
 		// Render a list using the "friends" being passed in.
+
 		return (
-			<div className="container">
-				{/* <Popular /> */}
-				<Battle />
-			</div>
+			<ThemeProvider value={this.state}>
+				<div className={this.state.theme}>
+					<div className="container">
+						<Nav />
+						{/* <Popular /> */}
+						<Battle />
+					</div>
+				</div>
+			</ThemeProvider>
 		);
 	}
 }
