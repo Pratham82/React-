960bfca4ba8fe21b7d1384e564c1a6a00f699283
Pratham82 <mali.prathamesh82@gmail.com>
2020-08-03 17:59:31 +0530

    Added NavLink

--- React (Tyler McGinnis course)/Gihub battle/src/components/Nav.js
+++ React (Tyler McGinnis course)/Gihub battle/src/components/Nav.js
@@ -1,11 +1,32 @@
 import React from "react";
 import { ThemeConsumer } from "../contexts/theme";
+import { NavLink } from "react-router-dom";
+
+const activeStyle = {
+	color: "rgb(187,46,31)",
+};
 
 export default function Nav() {
 	return (
 		<ThemeConsumer>
 			{({ theme, toggleTheme }) => (
 				<nav className="row space-between">
+					<ul className="row nav">
+						<li>
+							<NavLink to="/" exact activeStyle={activeStyle}>
+								Popular Repos
+							</NavLink>
+						</li>
+						<li>
+							<NavLink
+								to="/battle"
+								exact
+								activeStyle={activeStyle}
+							>
+								Battle
+							</NavLink>
+						</li>
+					</ul>
 					<button
 						style={{ fontSize: 30 }}
 						className="btn-clear"
