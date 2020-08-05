983d55d1959707d1089a79df8e1d83bf7a034ea2
Pratham82 <mali.prathamesh82@gmail.com>
2020-08-05 15:07:43 +0530

    Added dynamic import

--- React (Tyler McGinnis course)/Gihub battle/src/index.js
+++ React (Tyler McGinnis course)/Gihub battle/src/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Popular from "./components/Popular";
-import Battle from "./components/Battle";
+// import Popular from "./components/Popular";
+// import Battle from "./components/Battle";
+// import Results from "./components/Results";
 import { ThemeProvider } from "./contexts/theme";
 import Nav from "./components/Nav";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Results from "./components/Results";
+import Loading from "./components/Loading";
+
+const Popular = React.lazy(() => import("./components/Popular"));
+const Battle = React.lazy(() => import("./components/Battle"));
+const Results = React.lazy(() => import("./components/Results"));
 class App extends React.Component {
 	state = {
 		theme: "light",
@@ -30,23 +35,26 @@ class App extends React.Component {
 							<Nav />
 							{/* <Popular /> */}
 							{/* <Battle /> */}
-							<Switch>
-								<Route exact path="/" component={Popular} />
-								<Route
-									exact
-									path="/battle"
-									component={Battle}
-								/>
-								<Route
-									path="/battle/results"
-									component={Results}
-								/>
-								<Route
-									render={() => (
-										<h1>404 Page not found 🤷‍♂</h1>
-									)}
-								/>
-							</Switch>
+
+							<React.Suspense fallback={<Loading />}>
+								<Switch>
+									<Route exact path="/" component={Popular} />
+									<Route
+										exact
+										path="/battle"
+										component={Battle}
+									/>
+									<Route
+										path="/battle/results"
+										component={Results}
+									/>
+									<Route
+										render={() => (
+											<h1>404 Page not found 🤷‍♂</h1>
+										)}
+									/>
+								</Switch>
+							</React.Suspense>
 						</div>
 					</div>
 				</ThemeProvider>
