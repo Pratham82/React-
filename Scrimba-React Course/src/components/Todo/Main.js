3c5455408c2ad87409588c10e8609aeed2f25bcb
Pratham82 <mali.prathamesh82@gmail.com>
2020-08-08 18:35:29 +0530

    To do list updated with map

--- Scrimba-React Course/src/components/Todo/Main.js
+++ Scrimba-React Course/src/components/Todo/Main.js
@@ -1,14 +1,27 @@
 import React from "react";
 import TodoItem from "./TodoItem";
+import "../../App.css";
+
+import toDoData from "./To-doData";
+
+/*
+
+Challenge: Using the array map method, render a child component for each todo item in the todosData array and pass the relevant data to it.
+*/
 
 function Main() {
+	console.log(toDoData);
+
+	const ToDos = toDoData.map((todo) => (
+		<TodoItem key={todo.id} task={todo.text} progress={todo.completed} />
+	));
+
 	return (
-		<div className="todo-list">
-			<h1>To do list</h1>
-			<TodoItem />
-			<TodoItem />
-			<TodoItem />
-			<TodoItem />
+		<div className="App">
+			<div className="todo-list  App">
+				<h1>To do list</h1>
+				{ToDos}
+			</div>
 		</div>
 	);
 }
--- Scrimba-React Course/src/components/Todo/TodoItem.js
+++ Scrimba-React Course/src/components/Todo/TodoItem.js
@@ -1,10 +1,18 @@
 import React from "react";
 
-export default function TodoItem() {
+export default function TodoItem(props) {
+	console.log(props);
 	return (
 		<div className="todo-item">
-			<input type="checkbox" />
-			<p>Placeholder text here</p>
+			<input type="checkbox" checked={props.progress} />
+			{/* {props.progress ? (
+				<p>
+					<s>{props.task}</s>
+				</p>
+			) : (
+				<p>{props.task}</p>
+			)} */}
+			<p>{props.task}</p>
 		</div>
 	);
 }
