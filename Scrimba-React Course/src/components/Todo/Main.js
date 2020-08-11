25d18d8debcdb099f4115e766b58845449fd987e
Pratham82 <mali.prathamesh82@gmail.com>
2020-08-11 19:18:53 +0530

    Added class based component and states

--- Scrimba-React Course/src/components/Todo/Main.js
+++ Scrimba-React Course/src/components/Todo/Main.js
@@ -9,21 +9,40 @@ import toDoData from "./To-doData";
 Challenge: Using the array map method, render a child component for each todo item in the todosData array and pass the relevant data to it.
 */
 
-function Main() {
-	console.log(toDoData);
+/*
+In the previous iteration of this todo list app, we pulled in todos data from a JSON file and mapped over it to display the todo items.
 
-	const ToDos = toDoData.map((todo) => (
-		<TodoItem key={todo.id} task={todo.text} progress={todo.completed} />
-	));
+Eventually we'll want to be able to modify the data, which will only happen if we've "loaded" the data in to the component's state
 
-	return (
-		<div className="App">
-			<div className="todo-list  App">
-				<h1>To do list</h1>
-				{ToDos}
+Challenge: Change the <App /> component into a stateful class component and load the imported `todosData` into state.
+*/
+
+class Main extends React.Component {
+	constructor() {
+		super();
+
+		this.state = {
+			stateData: toDoData,
+		};
+	}
+	render() {
+		const ToDos = this.state.stateData.map((todo) => (
+			<TodoItem
+				key={todo.id}
+				task={todo.text}
+				progress={todo.completed}
+			/>
+		));
+
+		return (
+			<div className="App">
+				<div className="todo-list  App">
+					<h1>To do list</h1>
+					{ToDos}
+				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
 
 export default Main;
--- Scrimba-React Course/src/components/Todo/TodoItem.js
+++ Scrimba-React Course/src/components/Todo/TodoItem.js
@@ -1,10 +1,18 @@
 import React from "react";
 
+/**
+ * Challenge: Get rid of our warning about not having an onChange on our input. For now, the function that runs onChange can simply console.log something.
+ */
+
 export default function TodoItem(props) {
 	console.log(props);
 	return (
 		<div className="todo-item">
-			<input type="checkbox" checked={props.progress} />
+			<input
+				type="checkbox"
+				checked={props.progress}
+				onChange={() => console.log("checked")}
+			/>
 			{/* {props.progress ? (
 				<p>
 					<s>{props.task}</s>
