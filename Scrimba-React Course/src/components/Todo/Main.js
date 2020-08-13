fe4e29d50386ecf12452b291087ae898d22825b9
Pratham82 <mali.prathamesh82@gmail.com>
2020-08-14 00:12:44 +0530

    Todo page and styles changed

--- Scrimba-React Course/src/components/Todo/Main.js
+++ Scrimba-React Course/src/components/Todo/Main.js
@@ -3,19 +3,19 @@ import TodoItem from "./TodoItem";
 import "../../App.css";
 
 import toDoData from "./To-doData";
+import todosData from "./To-doData";
 
-/*
-
-Challenge: Using the array map method, render a child component for each todo item in the todosData array and pass the relevant data to it.
-*/
-
-/*
-In the previous iteration of this todo list app, we pulled in todos data from a JSON file and mapped over it to display the todo items.
-
-Eventually we'll want to be able to modify the data, which will only happen if we've "loaded" the data in to the component's state
-
-Challenge: Change the <App /> component into a stateful class component and load the imported `todosData` into state.
-*/
+/**
+ * Let's make it so our checkbox can actually mark our todo as complete or incomplete!
+ * This challenge is a little more involved than some of the past ones. Check the comments
+ * in the code for some help on accomplishing this one
+ *
+ * Challenge:
+ * 1. Create an event handler in the App component for when the checkbox is clicked (which is an `onChange` event)
+ *    a. This method will be the trickest part. Check the comments in the stubbed-out method below for some pseudocode to help guide you through this part
+ * 2. Pass the method down to the TodoItem component
+ * 3. In the TodoItem component, make it so when the `onChange` event happens, it calls the `handleChange` method and passes the id of the todo into the function
+ */
 
 class Main extends React.Component {
 	constructor() {
@@ -24,19 +24,49 @@ class Main extends React.Component {
 		this.state = {
 			stateData: toDoData,
 		};
+
+		this.handleChange = this.handleChange.bind(this);
+	}
+	handleChange(id) {
+		// Update state so that the item with the given id flips `completed` from false to true (or vise versa)s
+		// console.log(`This id was clicked ${todo.id}`);
+
+		// Remember not to modify prevState directly, but instead to return a new version of state with the change you want included in that update. (Think how you might use the `.map` method to do this)
+		this.setState((prevState) => {
+			const updatedState = prevState.stateData.map((todo) => {
+				if (todo.id === id) {
+					// Old way
+					// todo.completed = !todo.completed;
+
+					//* Instead of modifying the previous state we will return an object that will replace todo in new array and modifying the completed property rather than modifying the old object directly
+
+					return {
+						...todo,
+						completed: !todo.completed,
+					};
+				}
+				return todo;
+			});
+
+			return {
+				stateData: updatedState,
+			};
+		});
 	}
 	render() {
 		const ToDos = this.state.stateData.map((todo) => (
 			<TodoItem
 				key={todo.id}
+				taskID={todo.id}
 				task={todo.text}
 				progress={todo.completed}
+				handleChange={this.handleChange}
 			/>
 		));
 
 		return (
 			<div className="App">
-				<div className="todo-list  App">
+				<div className="todo-list App">
 					<h1>To do list</h1>
 					{ToDos}
 				</div>
--- Scrimba-React Course/src/components/Todo/TodoItem.js
+++ Scrimba-React Course/src/components/Todo/TodoItem.js
@@ -5,21 +5,14 @@ import React from "react";
  */
 
 export default function TodoItem(props) {
-	console.log(props);
+	// console.log(props.task.id);
 	return (
 		<div className="todo-item">
 			<input
 				type="checkbox"
 				checked={props.progress}
-				onChange={() => console.log("checked")}
+				onChange={() => props.handleChange(props.taskID)}
 			/>
-			{/* {props.progress ? (
-				<p>
-					<s>{props.task}</s>
-				</p>
-			) : (
-				<p>{props.task}</p>
-			)} */}
 			<p>{props.task}</p>
 		</div>
 	);
