0cb32c0b2585ef505eeb83736095698f757c2476
Pratham82 <mali.prathamesh82@gmail.com>
2020-08-31 20:29:22 +0530

    Type wise styling added

--- Modern React Bootcamp/05. Pokedex project/pokedex-2.0/src/PokeCard.jsx
+++ Modern React Bootcamp/05. Pokedex project/pokedex-2.0/src/PokeCard.jsx
@@ -1,28 +1,41 @@
 import React from "react";
+import "./PokeCard.css";
 
 export const PokeCard = (props) => {
 	const pokeData = props.pokeData;
-	console.log(pokeData);
-	const cardStyle = {
-		border: "1px solid",
-		padding: "5px 10px",
-		borderRadius: "8px",
-		fontSize: "18px",
-		fontFamily: "Fira code",
-		fontWeight: "400",
-		lineHeight: "0.5",
-		width: "200px",
-		height: "300px",
-		textAlign: "center",
-		margin: "10px",
-	};
+	// console.log(pokeData);
 
 	const padImg = (imgID) =>
 		imgID < 10 ? `00${imgID}` : imgID < 100 ? `0${imgID}` : `${imgID}`;
+
+	const colors = {
+		fire: "#FDDFDF",
+		grass: "#DEFDE0",
+		electric: "#FCF7DE",
+		water: "#DEF3FD",
+		ground: "#f4e7da",
+		rock: "#d5d5d4",
+		fairy: "#fceaff",
+		poison: "#98d7a5",
+		bug: "#f8d5a3",
+		dragon: "#97b3e6",
+		psychic: "#eaeda1",
+		flying: "#F5F5F5",
+		fighting: "#E6E0D4",
+		normal: "#F5F5F5",
+	};
+
+	const main_types = Object.keys(colors);
+
 	return (
-		<div style={cardStyle}>
+		<div
+			key={pokeData.id}
+			className="cardStyle"
+			style={{ backgroundColor: colors[pokeData.type] }}
+		>
 			<p>#{pokeData.id}</p>
 			<img
+				className="cardImg"
 				src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${padImg(
 					pokeData.id
 				)}.png`}
