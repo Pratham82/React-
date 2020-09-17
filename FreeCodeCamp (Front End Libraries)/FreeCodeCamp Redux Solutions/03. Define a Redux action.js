5f3e46a194afe05c33a93fdbcf9d80901680e382
Pratham82 <mali.prathamesh82@gmail.com>
2020-09-18 02:41:09 +0530

    Completed 11 questions from Redux

---
+++ FreeCodeCamp (Front End Libraries)/FreeCodeCamp Redux Solutions/03. Define a Redux action.js
@@ -0,0 +1,5 @@
+// Define a Redux ActionPassed
+
+// Writing a Redux action is as simple as declaring an object with a type property. Declare an object action and give it a property type set to the string 'LOGIN'.
+
+let action = { type: "LOGIN" };
