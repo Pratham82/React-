00aed01e69403b9b12fa644901ced1f9a35ce678
Pratham82 <mali.prathamesh82@gmail.com>
2020-09-18 17:57:44 +0530

    Completed week 1 assignments

--- Front end Web Developement with React (Coursera)/Week-1/react-starter/src/components/MenuComponent.js
+++ Front end Web Developement with React (Coursera)/Week-1/react-starter/src/components/MenuComponent.js
@@ -1,75 +1,51 @@
 import React, { Component } from "react";
-import { Media } from "reactstrap";
+import {
+	Media,
+	Card,
+	CardImg,
+	CardImgOverlay,
+	CardBody,
+	CardTitle,
+	CardText,
+} from "reactstrap";
+import dishes from "../shared/dishes";
+import DishDetail from "./DishDetailComponent";
 
 export default class Menu extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {
-			dishes: [
-				{
-					id: 0,
-					name: "Uthappizza",
-					image: "assets/images/uthappizza.png",
-					category: "mains",
-					label: "Hot",
-					price: "4.99",
-					description:
-						"A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.",
-				},
-				{
-					id: 1,
-					name: "Zucchipakoda",
-					image: "assets/images/zucchipakoda.png",
-					category: "appetizer",
-					label: "",
-					price: "1.99",
-					description:
-						"Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce",
-				},
-				{
-					id: 2,
-					name: "Vadonut",
-					image: "assets/images/vadonut.png",
-					category: "appetizer",
-					label: "New",
-					price: "1.99",
-					description:
-						"A quintessential ConFusion experience, is it a vada or is it a donut?",
-				},
-				{
-					id: 3,
-					name: "ElaiCheese Cake",
-					image: "assets/images/elaicheesecake.png",
-					category: "dessert",
-					label: "",
-					price: "2.99",
-					description:
-						"A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms",
-				},
-			],
-		};
+		this.state = { dishes, selectedDish: null };
 	}
+
+	onDishSelect = (dish) => {
+		this.setState({
+			selectedDish: dish,
+		});
+	};
 
 	render() {
 		const menu = this.state.dishes.map((item) => (
-			<div key={item.id} className="col-12 mt-5">
-				<Media tag="li">
-					<Media left middle>
-						<Media object src={item.image} alt={item.name}></Media>
-					</Media>
-					<Media body className="ml-5">
-						<Media heading>{item.name}</Media>
-						<p>{item.description}</p>
-					</Media>
-				</Media>
+			<div key={item.id} className="col-12 col-md-5 m-1">
+				<Card onClick={() => this.onDishSelect(item)}>
+					<CardImg
+						object
+						src={item.image}
+						alt={item.name}
+						width="100%"
+					/>
+					<CardImgOverlay>
+						<CardBody>
+							<CardTitle>{item.name}</CardTitle>
+						</CardBody>
+					</CardImgOverlay>
+				</Card>
 			</div>
 		));
-		console.log(menu);
+
 		return (
 			<div className="container">
-				<div className="row">
-					<Media list>{menu}</Media>
-				</div>
+				<div className="row">{menu}</div>
+				<DishDetail dish={this.state.selectedDish} />
 			</div>
 		);
 	}
