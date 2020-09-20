76fe8490a4dedf285c2792975fdf2078fe432aff
Pratham82 <mali.prathamesh82@gmail.com>
2020-09-20 12:14:47 +0530

    Alert box modified

--- Front end Web Developement with React (Coursera)/Week-3/src/components/HeaderComponent.js
+++ Front end Web Developement with React (Coursera)/Week-3/src/components/HeaderComponent.js
@@ -33,12 +33,7 @@ class Header extends Component {
 	handleLogin = (event) => {
 		this.toggleModal();
 		alert(
-			"Username: " +
-				this.username.value +
-				" Password: " +
-				this.password.value +
-				" Remember: " +
-				this.remember.checked
+			`Username: ${this.username.value}  Password: ${this.password.value} Remember: ${this.remember.checked}`
 		);
 		event.preventDefault();
 	};
