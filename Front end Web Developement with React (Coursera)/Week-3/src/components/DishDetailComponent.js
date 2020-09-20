18e3f9b94c9b3fb5d1baa0e6578de197cf24cdbf
Pratham82 <mali.prathamesh82@gmail.com>
2020-09-20 15:42:21 +0530

    Week Adsignment 3 completed

--- Front end Web Developement with React (Coursera)/Week-3/src/components/DishDetailComponent.js
+++ Front end Web Developement with React (Coursera)/Week-3/src/components/DishDetailComponent.js
@@ -1,14 +1,151 @@
-import React from "react";
+import React, { Component } from "react";
 import {
 	Card,
 	CardImg,
-	CardBody,
-	CardTitle,
 	CardText,
+	CardTitle,
+	CardBody,
 	Breadcrumb,
 	BreadcrumbItem,
+	Modal,
+	ModalBody,
+	ModalHeader,
+	Button,
+	Row,
+	Col,
+	Label,
 } from "reactstrap";
+import { Control, LocalForm, Errors } from "react-redux-form";
 import { Link } from "react-router-dom";
+import "font-awesome/css/font-awesome.min.css";
+
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !val || val.length <= len;
+const minLength = (len) => (val) => val && val.length >= len;
+
+class CommentForm extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			modalOpen: false,
+		};
+	}
+
+	handleToggle = () => this.setState({ modalOpen: !this.state.modalOpen });
+
+	submitHandle = (values) => {
+		this.handleToggle(values);
+		alert(`Current state is: ${JSON.stringify(values)}`);
+	};
+
+	render() {
+		return (
+			<>
+				<Button
+					className="bg-white text-dark"
+					onClick={this.handleToggle}
+				>
+					<i className="fa fa-pencil fa-lg"></i> Submit Comment
+				</Button>
+				<Modal isOpen={this.state.modalOpen} toggle={this.handleToggle}>
+					<ModalHeader toggle={this.handleToggle}>
+						Submit Comment
+					</ModalHeader>
+					<ModalBody>
+						<LocalForm
+							onSubmit={(value) => this.submitHandle(value)}
+						>
+							<Row className="form-group">
+								<Label htmlFor="rating" md={4}>
+									Rating
+								</Label>
+								<Col md={12}>
+									<Control.select
+										model=".rating"
+										id="rating"
+										name="rating"
+										className="form-control"
+									>
+										<option>1</option>
+										<option>2</option>
+										<option>3</option>
+										<option>4</option>
+										<option>5</option>
+									</Control.select>
+								</Col>
+							</Row>
+							<Row className="form-group">
+								<Label htmlFor="author" md={4}>
+									Your Name
+								</Label>
+								<Col md={12}>
+									<Control.text
+										model=".author"
+										id="author"
+										name="author"
+										placeholder="Your Name"
+										className="form-control"
+										validators={{
+											minLength: minLength(3),
+											maxLength: maxLength(15),
+										}}
+									/>
+									<Errors
+										className="text-danger"
+										model=".author"
+										show="touched"
+										messages={{
+											minLength:
+												"Must be greater than 2 characters",
+											maxLength:
+												"Must be 15 characters or less",
+										}}
+									/>
+								</Col>
+							</Row>
+							<Row className="form-group">
+								<Label htmlFor="comment" md={4}>
+									Comment
+								</Label>
+								<Col md={12}>
+									<Control.textarea
+										model=".comment"
+										id="comment"
+										name="comment"
+										className="form-control"
+										validators={{
+											required,
+										}}
+										rows="6"
+									/>
+									<Errors
+										className="text-danger"
+										model=".comment"
+										show="touched"
+										messages={{
+											required: "Comment Required",
+										}}
+									/>
+								</Col>
+							</Row>
+							<Row className="form-group">
+								<Col>
+									<Button
+										type="submit"
+										color="primary"
+										onSubmit={this.showReview}
+									>
+										Submit
+									</Button>
+								</Col>
+							</Row>
+						</LocalForm>
+					</ModalBody>
+				</Modal>
+			</>
+		);
+	}
+}
 
 //* Functional components
 const RenderDish = ({ dish }) =>
@@ -46,6 +183,7 @@ const RenderComments = ({ comment }) =>
 						<br />
 					</React.Fragment>
 				))}
+				<CommentForm />
 			</ul>
 		</div>
 	) : (
