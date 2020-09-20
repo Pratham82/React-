d26467bd5d99321306011d96cdf8fc036b7a1dd8
Pratham82 <mali.prathamesh82@gmail.com>
2020-09-20 12:04:17 +0530

    Completed uncontrolled form

--- Front end Web Developement with React (Coursera)/Week-3/src/components/HeaderComponent.js
+++ Front end Web Developement with React (Coursera)/Week-3/src/components/HeaderComponent.js
@@ -7,6 +7,14 @@ import {
 	Collapse,
 	NavItem,
 	Jumbotron,
+	Button,
+	Modal,
+	ModalHeader,
+	ModalBody,
+	FormGroup,
+	Form,
+	Label,
+	Input,
 } from "reactstrap";
 import { NavLink } from "react-router-dom";
 
@@ -15,9 +23,25 @@ class Header extends Component {
 		super(props);
 		this.state = {
 			isNavOpen: false,
+			isModalOpen: false,
 		};
 	}
 	toggleNav = () => this.setState({ isNavOpen: !this.state.isNavOpen });
+
+	toggleModal = () => this.setState({ isModalOpen: !this.state.isModalOpen });
+
+	handleLogin = (event) => {
+		this.toggleModal();
+		alert(
+			"Username: " +
+				this.username.value +
+				" Password: " +
+				this.password.value +
+				" Remember: " +
+				this.remember.checked
+		);
+		event.preventDefault();
+	};
 
 	render() {
 		return (
@@ -64,6 +88,17 @@ class Header extends Component {
 									</NavLink>
 								</NavItem>
 							</Nav>
+							<Nav className="ml-auto">
+								<NavItem>
+									<Button
+										color="primary"
+										onClick={this.toggleModal}
+									>
+										<span className="fa fa-sign-in  fa-lg"></span>{" "}
+										&nbsp; Log In
+									</Button>
+								</NavItem>
+							</Nav>
 						</Collapse>
 					</div>
 				</Navbar>
@@ -82,6 +117,57 @@ class Header extends Component {
 						</div>
 					</div>
 				</Jumbotron>
+				<Modal
+					isOpen={this.state.isModalOpen}
+					toggle={this.toggleModal}
+				>
+					<ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+					<ModalBody>
+						<Form onSubmit={this.handleLogin}>
+							<FormGroup>
+								<Label htmlFor="username">Username</Label>
+								<Input
+									type="text"
+									id="username"
+									name="username"
+									innerRef={(input) =>
+										(this.username = input)
+									}
+								></Input>
+							</FormGroup>
+							<FormGroup>
+								<Label htmlFor="password">Password</Label>
+								<Input
+									type="password"
+									id="password"
+									name="password"
+									innerRef={(input) =>
+										(this.password = input)
+									}
+								></Input>
+							</FormGroup>
+							<FormGroup check className="m-2">
+								<Label check>
+									<Input
+										type="checkbox"
+										name="remember"
+										innerRef={(input) =>
+											(this.remember = input)
+										}
+									></Input>
+									Remember Me
+								</Label>
+							</FormGroup>
+							<Button
+								type="submit"
+								value="submit"
+								color="primary"
+							>
+								Login
+							</Button>
+						</Form>
+					</ModalBody>
+				</Modal>
 			</>
 		);
 	}
