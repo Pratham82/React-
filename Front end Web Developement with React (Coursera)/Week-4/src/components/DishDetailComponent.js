efe71c92fc9c25db1fedf30d8c8ad994f359f352
Pratham82 <mali.prathamesh82@gmail.com>
2020-09-21 13:08:08 +0530

    Completed Redux actions section

---
+++ Front end Web Developement with React (Coursera)/Week-4/src/components/DishDetailComponent.js
@@ -0,0 +1,233 @@
+import React, { Component } from "react";
+import {
+	Card,
+	CardImg,
+	CardText,
+	CardTitle,
+	CardBody,
+	Breadcrumb,
+	BreadcrumbItem,
+	Modal,
+	ModalBody,
+	ModalHeader,
+	Button,
+	Row,
+	Col,
+	Label,
+} from "reactstrap";
+import { Control, LocalForm, Errors } from "react-redux-form";
+import { Link } from "react-router-dom";
+
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !val || val.length <= len;
+const minLength = (len) => (val) => val && val.length >= len;
+
+class CommentForm extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			modalOpen: false,
+		};
+	}
+
+	handleToggle = () => this.setState({ modalOpen: !this.state.modalOpen });
+
+	submitHandle = (values) => {
+		this.handleToggle(values);
+		// alert(`Current state is: ${JSON.stringify(values)}`);
+		this.props.addComments(
+			this.props.dishId,
+			values.rating,
+			values.author,
+			values.comment
+		);
+	};
+
+	render() {
+		return (
+			<>
+				<Button
+					className="bg-white text-dark"
+					onClick={this.handleToggle}
+				>
+					<i className="fa fa-pencil fa-lg"></i> Submit Comment
+				</Button>
+				<Modal isOpen={this.state.modalOpen} toggle={this.handleToggle}>
+					<ModalHeader toggle={this.handleToggle}>
+						Submit Comment
+					</ModalHeader>
+					<ModalBody>
+						<LocalForm
+							onSubmit={(value) => this.submitHandle(value)}
+						>
+							<Row className="form-group">
+								<Label htmlFor="rating" md={4}>
+									Rating
+								</Label>
+								<Col md={12}>
+									<Control.select
+										model=".rating"
+										id="rating"
+										name="rating"
+										className="form-control"
+									>
+										<option>1</option>
+										<option>2</option>
+										<option>3</option>
+										<option>4</option>
+										<option>5</option>
+									</Control.select>
+								</Col>
+							</Row>
+							<Row className="form-group">
+								<Label htmlFor="author" md={4}>
+									Your Name
+								</Label>
+								<Col md={12}>
+									<Control.text
+										model=".author"
+										id="author"
+										name="author"
+										placeholder="Your Name"
+										className="form-control"
+										validators={{
+											minLength: minLength(3),
+											maxLength: maxLength(15),
+										}}
+									/>
+									<Errors
+										className="text-danger"
+										model=".author"
+										show="touched"
+										messages={{
+											minLength:
+												"Must be greater than 2 characters",
+											maxLength:
+												"Must be 15 characters or less",
+										}}
+									/>
+								</Col>
+							</Row>
+							<Row className="form-group">
+								<Label htmlFor="comment" md={4}>
+									Comment
+								</Label>
+								<Col md={12}>
+									<Control.textarea
+										model=".comment"
+										id="comment"
+										name="comment"
+										className="form-control"
+										validators={{
+											required,
+										}}
+										rows="6"
+									/>
+									<Errors
+										className="text-danger"
+										model=".comment"
+										show="touched"
+										messages={{
+											required: "Comment Required",
+										}}
+									/>
+								</Col>
+							</Row>
+							<Row className="form-group">
+								<Col>
+									<Button
+										type="submit"
+										color="primary"
+										onSubmit={this.showReview}
+									>
+										Submit
+									</Button>
+								</Col>
+							</Row>
+						</LocalForm>
+					</ModalBody>
+				</Modal>
+			</>
+		);
+	}
+}
+
+//* Functional components
+const RenderDish = ({ dish }) =>
+	dish !== undefined ? (
+		<React.Fragment>
+			<div className="col-12 col-md-5 m-1">
+				<Card>
+					<CardImg
+						object
+						src={dish.image}
+						alt={dish.name}
+						width="100%"
+					/>
+					<CardBody>
+						<CardTitle>{dish.name}</CardTitle>
+						<CardText>{dish.description}</CardText>
+					</CardBody>
+				</Card>
+			</div>
+		</React.Fragment>
+	) : (
+		<div></div>
+	);
+
+const RenderComments = ({ comment, addComments, dishId }) =>
+	comment !== null ? (
+		<div className="col-12 col-md-5 m-1">
+			<h4>Comments</h4>
+			<ul className="list-unstyled">
+				{comment.map((c) => (
+					<React.Fragment key={c.id}>
+						<li>{c.comment}</li>
+						<br />
+						<li>{`-- ${c.author}, ${dateFormatter(c.date)}`}</li>
+						<br />
+					</React.Fragment>
+				))}
+				<CommentForm dishId={dishId} addComments={addComments} />
+			</ul>
+		</div>
+	) : (
+		<div></div>
+	);
+
+const dateFormate = {
+	year: "numeric",
+	month: "short",
+	day: "numeric",
+};
+const dateFormatter = (date) =>
+	new Date(date).toLocaleDateString("en-US", dateFormate);
+
+const DishDetail = (props) => (
+	<div className="container">
+		<div className="row">
+			<div className="row">
+				<Breadcrumb>
+					<BreadcrumbItem>
+						<Link to="/menu">Menu</Link>
+					</BreadcrumbItem>
+					<BreadcrumbItem>{props.dish.name}</BreadcrumbItem>
+				</Breadcrumb>
+				<div className="col-12">
+					<h3>{props.dish.name}</h3>
+					<hr />
+				</div>
+				<div className="row">
+					<RenderDish dish={props.dish} />
+					<RenderComments
+						comment={props.comments}
+						addComments={props.addComments}
+						dishId={props.dish.id}
+					/>
+				</div>
+			</div>
+		</div>
+	</div>
+);
+
+export default DishDetail;
---
+++ Front end Web Developement with React (Coursera)/Week-4/src/components/HeaderComponent.js
@@ -0,0 +1,171 @@
+import React, { Component } from "react";
+import {
+	Navbar,
+	NavbarBrand,
+	Nav,
+	NavbarToggler,
+	Collapse,
+	NavItem,
+	Jumbotron,
+	Button,
+	Modal,
+	ModalHeader,
+	ModalBody,
+	FormGroup,
+	Form,
+	Label,
+	Input,
+} from "reactstrap";
+import { NavLink } from "react-router-dom";
+
+class Header extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isNavOpen: false,
+			isModalOpen: false,
+		};
+	}
+	toggleNav = () => this.setState({ isNavOpen: !this.state.isNavOpen });
+
+	toggleModal = () => this.setState({ isModalOpen: !this.state.isModalOpen });
+
+	handleLogin = (event) => {
+		this.toggleModal();
+		alert(
+			`Username: ${this.username.value}  Password: ${this.password.value} Remember: ${this.remember.checked}`
+		);
+		event.preventDefault();
+	};
+
+	render() {
+		return (
+			<>
+				<Navbar dark expand="md">
+					<div className="container">
+						<NavbarToggler onClick={this.toggleNav} />
+						<NavbarBrand href="/" className="mr-auto">
+							<img
+								src="assets/images/logo.png"
+								alt="Ristorante ConFusion"
+								height="30"
+								width="41"
+							/>
+						</NavbarBrand>
+						<Collapse navbar isOpen={this.state.isNavOpen}>
+							<Nav navbar>
+								<NavItem>
+									<NavLink className="nav-link" to="/home">
+										<span className="fa fa-home fa-lg">
+											&nbsp; Home
+										</span>
+									</NavLink>
+								</NavItem>
+								<NavItem>
+									<NavLink className="nav-link" to="/menu">
+										<span className="fa fa-list fa-lg">
+											&nbsp; Menu
+										</span>
+									</NavLink>
+								</NavItem>
+								<NavItem>
+									<NavLink className="nav-link" to="/about">
+										<span className="fa fa-info fa-lg">
+											&nbsp; About Us
+										</span>
+									</NavLink>
+								</NavItem>
+								<NavItem>
+									<NavLink className="nav-link" to="/contact">
+										<span className="fa fa-address-card fa-lg">
+											&nbsp; Contact
+										</span>
+									</NavLink>
+								</NavItem>
+							</Nav>
+							<Nav className="ml-auto">
+								<NavItem>
+									<Button
+										color="primary"
+										onClick={this.toggleModal}
+									>
+										<span className="fa fa-sign-in  fa-lg"></span>{" "}
+										&nbsp; Log In
+									</Button>
+								</NavItem>
+							</Nav>
+						</Collapse>
+					</div>
+				</Navbar>
+				<Jumbotron>
+					<div className="container">
+						<div className="row row-header">
+							<div className="col-12 col-sm-6">
+								<h1>Ristorante ConFusion</h1>{" "}
+								<p>
+									We take inspiration from the World's best
+									cuisines, and create a unique fusion
+									experience. Our lipsmacking creations will
+									tickle your culinary senses!
+								</p>
+							</div>
+						</div>
+					</div>
+				</Jumbotron>
+				<Modal
+					isOpen={this.state.isModalOpen}
+					toggle={this.toggleModal}
+				>
+					<ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+					<ModalBody>
+						<Form onSubmit={this.handleLogin}>
+							<FormGroup>
+								<Label htmlFor="username">Username</Label>
+								<Input
+									type="text"
+									id="username"
+									name="username"
+									innerRef={(input) =>
+										(this.username = input)
+									}
+								></Input>
+							</FormGroup>
+							<FormGroup>
+								<Label htmlFor="password">Password</Label>
+								<Input
+									type="password"
+									id="password"
+									name="password"
+									innerRef={(input) =>
+										(this.password = input)
+									}
+								></Input>
+							</FormGroup>
+							<FormGroup check className="m-2">
+								<Label check>
+									<Input
+										type="checkbox"
+										name="remember"
+										innerRef={(input) =>
+											(this.remember = input)
+										}
+									></Input>
+									Remember Me
+								</Label>
+							</FormGroup>
+							<Button
+								type="submit"
+								value="submit"
+								color="primary"
+							>
+								Login
+							</Button>
+						</Form>
+					</ModalBody>
+				</Modal>
+			</>
+		);
+	}
+}
+
+export default Header;
