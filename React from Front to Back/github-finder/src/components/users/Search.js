28580f40046b3a183e7b22b02be0d27af780ed6a
pratham82 <mali.prathamesh82@gmail.com>
2020-10-29 00:09:57 +0530

    Added clear button

--- React from Front to Back/github-finder/src/components/users/Search.js
+++ React from Front to Back/github-finder/src/components/users/Search.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react"
+import PropTypes from "prop-types"
 
 export default class Search extends Component {
   state = {
     text: "",
+  }
+
+  static propTypes = {
+    searchUser: PropTypes.func.isRequired,
+    clearUser: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
   }
 
   handleChange = (e) => this.setState({ [e.target.name]: e.target.value })
@@ -13,22 +20,33 @@ export default class Search extends Component {
     this.setState({ text: "" })
   }
 
+  handleClick = () => console.log("check")
+
   render() {
+    const { showClear, clearUser } = this.props
+
     return (
-      <form className="form" onSubmit={this.handleSubmit}>
-        <input
-          type="text"
-          name="text"
-          value={this.state.text}
-          placeholder="Search Usernames"
-          onChange={this.handleChange}
-        />
-        <input
-          type="submit"
-          value="Search"
-          className="btn btn-dark btn-block"
-        />
-      </form>
+      <div>
+        <form className="form" onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="text"
+            value={this.state.text}
+            placeholder="Search Usernames"
+            onChange={this.handleChange}
+          />
+          <input
+            type="submit"
+            value="Search"
+            className="btn btn-dark btn-block"
+          />
+        </form>
+        {showClear && (
+          <button className="btn btn-light btn-block" onClick={clearUser}>
+            Clear
+          </button>
+        )}
+      </div>
     )
   }
 }
