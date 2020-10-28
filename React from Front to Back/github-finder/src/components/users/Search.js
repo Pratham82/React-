76fb575cca8cbcc8c6b404697d3a496f96a24950
pratham82 <mali.prathamesh82@gmail.com>
2020-10-28 21:53:36 +0530

    Created search Component

---
+++ React from Front to Back/github-finder/src/components/users/Search.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react"
+
+export default class Search extends Component {
+  state = {
+    text: "",
+  }
+
+  handleChange = (e) => this.setState({ [e.target.name]: e.target.value })
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.searchUser(this.state.text)
+    this.setState({ text: "" })
+  }
+
+  handleKeyPress = (e) => {
+    this.props.searchUser(this.state.text)
+  }
+
+  render() {
+    return (
+      <form className="form" onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          name="text"
+          value={this.state.text}
+          placeholder="Search Usernames"
+          onChange={this.handleChange}
+          onKeyUp={this.handleKeyPress}
+        />
+        <input
+          type="submit"
+          value="Search"
+          className="btn btn-dark btn-block"
+        />
+      </form>
+    )
+  }
+}
