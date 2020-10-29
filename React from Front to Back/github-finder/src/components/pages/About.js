664ee736b39495bcb54eadede2cf4ad7412518f6
pratham82 <mali.prathamesh82@gmail.com>
2020-10-29 11:45:57 +0530

    Created About page

---
+++ React from Front to Back/github-finder/src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const About = () => (
+  <div style={{ textAlign: "center" }}>
+    <h1>About this App</h1>
+    <p>This is the App to search Gtihub users information</p>
+    <p>Version 1.0.0</p>
+  </div>
+)
+
+export default About
