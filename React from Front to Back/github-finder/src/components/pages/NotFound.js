d57a1e54cabfb2164f602778d5255b4dba51d8c8
pratham82 <mali.prathamesh82@gmail.com>
2020-10-31 17:27:33 +0530

    Created Home and NotFound page

---
+++ React from Front to Back/github-finder/src/components/pages/NotFound.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const NotFound = () => (
+  <div className="text-center">
+    <h1>404: Page Not Found</h1>
+    <p className="lead">Page that you are looking for does not exists</p>{" "}
+  </div>
+)
+
+export default NotFound
